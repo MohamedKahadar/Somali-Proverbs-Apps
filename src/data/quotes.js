4253d464f24a83e4f04a56f4c8bd9a484c164846
MohamedKahadar <faaraxmaxamadm7@gmail.com>
2024-07-19 15:16:44 +0300

    new proverbs added

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -5,7 +5,23 @@ const Quotes=[
     "nin seexday sedkiisuu helaa",
     "nin sabool ah sabool maahan",
     "samo ku joogso saameyn ku yeelan",
-    "sadar qalalaase ma aha sadaal"
+    "sadar qalalaase ma aha sadaal",
+    "Baadi nimaan lahayn bay ag joogtaa",
+    "Baahiyi qof kuu daran bay kuu geysaa",
+    "Badhi mag ma laha",
+"Balaayo laba qad ma leh",
+"Balaayo madax la qabtay leedahaye minjo la qabto ma laha",
+"Balaayo buulkaagaa laga galaa",
+"Ballan habeenimo, jarmaado subax buu leeyahay",
+"Ballaysin bir ka adag",
+"Ban waraabiyo gaala baqataba baraa la helaa",
+"Bannaankaba arkaayow maxaa babac ku saaraaya",
+"Bannaankiis mare maradiisa geed ma qabsado",
+"Baqal fardo la miratay dhigood bay is moodaa",
+"Barasho horteed dad ha nicin",
+"Barasho horteed ha i nicin",
+    
+
 ]
 
 export const randomQuote = () => {
